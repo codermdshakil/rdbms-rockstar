52601eafb60c6dce5cb5c582f41179d94f9afac8
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-22 21:34:36 +0600

    Segment - 1: Alter, Select

--- module-10.5-segment-1/quries.sql
+++ module-10.5-segment-1/quries.sql
@@ -1,2 +1,20 @@
 -- Active: 1747835496638@@127.0.0.1@5432@b5_psql_cs
- 
+
+-- add column
+ALTER Table books   
+    ADD COLUMN genre TEXT DEFAULT;
+
+-- delete column
+ALTER TABLE books
+    drop COLUMN genre;
+
+ALTER Table books
+    alter COLUMN in_stock TYPE text;
+
+ALTER Table books 
+    Add COLUMN in_stock BOOLEAN DEFAULT true;
+
+
+select * from books;
+
+SELECT title, price FROM books;
